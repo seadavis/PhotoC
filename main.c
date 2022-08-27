6481e7b29749eec46a396ca5d8465c080f527fb2
seadav.17@gmail.com <seadav.17@gmail.com>
2022-08-26 21:33:50 -0700

    First experiments in capturing

--- main.c
+++ main.c
@@ -24,55 +24,7 @@
 
 #include "samples.h"
 
-static int
-_lookup_widget(CameraWidget*widget, const char *key, CameraWidget **child) {
-	int ret;
-	ret = gp_widget_get_child_by_name (widget, key, child);
-	if (ret < GP_OK)
-		ret = gp_widget_get_child_by_label (widget, key, child);
-	return ret;
-}
 
-int
-camera_eosviewfinder(Camera *camera, GPContext *context, int onoff) {
-	CameraWidget		*widget = NULL, *child = NULL;
-	int			ret,val;
-
-	ret = gp_camera_get_config (camera, &widget, context);
-	if (ret < GP_OK) {
-		fprintf (stderr, "camera_get_config failed: %d\n", ret);
-		return ret;
-	}
-	
-	ret = _lookup_widget (widget, "LiveViewMovieMode", &child);
-	if (ret < GP_OK) {
-		fprintf (stderr, "lookup 'LiveViewMode' failed: %d\n", ret);
-		goto out;
-	}
-
-
-	
-	ret = gp_widget_get_value (child, &val);
-	if (ret < GP_OK) {
-		fprintf (stderr, "could not get widget value: %d\n", ret);
-		goto out;
-	}
-	val = onoff;
-	ret = gp_widget_set_value (child, &val);
-	if (ret < GP_OK) {
-		fprintf (stderr, "could not set widget value to 1: %d\n", ret);
-		goto out;
-	}
-
-	ret = gp_camera_set_config (camera, widget, context);
-	if (ret < GP_OK) {
-		fprintf (stderr, "could not set config tree to eosviewfinder: %d\n", ret);
-		goto out;
-	}
-out:
-	gp_widget_free (widget);
-	return ret;
-}
 
 static void errordumper(GPLogLevel level, const char *domain, const char *str,
                  void *data) {
@@ -85,25 +37,28 @@ capture_to_memory(Camera *camera, GPContext *context, const char **ptr, unsigned
 	CameraFile *file;
 	CameraFilePath camera_file_path;
 
-	printf("Capturing Preview.\n");
+	printf("Capturing.\n");
 
-    camera_eosviewfinder(camera, context, 1);
+	/* NOP: This gets overridden in the library to /capt0000.jpg */
+	strcpy(camera_file_path.folder, "/");
+	strcpy(camera_file_path.name, "foo.jpg");
 
-    retval = gp_file_new(&file);
-    printf(" Retval From file new: %d\n", retval);
-    retval = gp_file_set_name(file, "preview.jpg");
-   
-	retval = gp_camera_capture_preview(camera, file, context);
-	printf(" Retval From Capture preview: %d\n", retval);
+	for(int i = 0; i < 10; i++)
+	{
+		retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &camera_file_path, context);
+		printf("  Retval: %d\n", retval);
+	}
+	
 
 	printf("Pathname on the camera: %s/%s\n", camera_file_path.folder, camera_file_path.name);
 
-	for(int i = 0; i < 100; i++)
-	{
-		gp_file_get_data_and_size (file, ptr, size);
-		printf("Got Data of Size %lu\n", *size);
-	}
-	
+	retval = gp_file_new(&file);
+	printf("  Retval: %d\n", retval);
+	retval = gp_camera_file_get(camera, camera_file_path.folder, camera_file_path.name,
+		     GP_FILE_TYPE_NORMAL, file, context);
+	printf("  Retval: %d\n", retval);
+
+	gp_file_get_data_and_size (file, ptr, size);
 
 	printf("Deleting.\n");
 	retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
@@ -119,12 +74,16 @@ capture_to_memory(Camera *camera, GPContext *context, const char **ptr, unsigned
         }
     }*/
 
+/*
+ This kind of works I do need to add a wait for 2 seconds.
+ I'd also like to know wtf it is returning
+*/
 int
 main(int argc, char **argv) {
 	Camera	*camera;
 	int	retval;
 	GPContext *context = sample_create_context();
-	//FILE 	*f;
+	FILE 	*f;
 	char	*data;
 	unsigned long size;
 
@@ -150,7 +109,7 @@ main(int argc, char **argv) {
 	capture_to_memory(camera, context, (const char**)&data, &size);
     //print_char_array(data, size);
 
-	/*f = fopen("foo2.jpg", "wb");
+	f = fopen("foo2.", "wb");
 	if (f) {
 		retval = fwrite (data, size, 1, f);
 		if (retval != (int)size) {
@@ -158,7 +117,7 @@ main(int argc, char **argv) {
 		}
 		fclose(f);
 	} else
-		printf("  fopen foo2.jpg failed.\n");*/
+		printf("  fopen foo2.jpg failed.\n");
 	gp_camera_exit(camera, context);
 	return 0;
 }
