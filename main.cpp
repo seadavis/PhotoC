c3ae5520a4fc1285e9cbf7d55d378bd1180945ec
seadav.17@gmail.com <seadav.17@gmail.com>
2022-08-28 22:37:34 -0700

    Getting displaying on OpenCV to work

--- main.cpp
+++ main.cpp
@@ -88,35 +88,31 @@ static void errordumper(GPLogLevel level, const char *domain, const char *str,
 }
 
 static void
-capture_to_memory(Camera *camera, GPContext *context, const char **ptr, unsigned long int *size) {
+capture_to_memory(Camera *camera, GPContext *context, CameraFilePath* camera_file_path, CameraFile *file, const char **ptr, unsigned long int *size) {
 	int retval;
-	CameraFile *file;
-	CameraFilePath camera_file_path;
+	
 
 	printf("Capturing.\n");
 
 	/* NOP: This gets overridden in the library to /capt0000.jpg */
-	strcpy(camera_file_path.folder, "/");
-	strcpy(camera_file_path.name, "foo.jpg");
+	strcpy(camera_file_path->folder, "/");
+	strcpy(camera_file_path->name, "foo.jpg");
 
-	retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &camera_file_path, context);
+	retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, camera_file_path, context);
 	printf("  Retval: %d\n", retval);
 
-	printf("Pathname on the camera: %s/%s\n", camera_file_path.folder, camera_file_path.name);
+	printf("Pathname on the camera: %s/%s\n", camera_file_path->folder, 
+                                                camera_file_path->name);
 
 	retval = gp_file_new(&file);
 	printf("  Retval: %d\n", retval);
-	retval = gp_camera_file_get(camera, camera_file_path.folder, camera_file_path.name,
+	retval = gp_camera_file_get(camera, camera_file_path->folder, camera_file_path->name,
 		     GP_FILE_TYPE_NORMAL, file, context);
 	printf("  Retval: %d\n", retval);
 
+  
 	gp_file_get_data_and_size (file, ptr, size);
 
-	printf("Deleting.\n");
-    // I wonder would it work without this?
-	retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
-			context);
-	printf("  Retval: %d\n", retval);
 	/*gp_file_free(file); */
 }
 
@@ -193,11 +189,11 @@ int
 main(int argc, char **argv) {
 
 	Camera	*camera;
+    CameraFile *cameraFile;
+    CameraFilePath camera_file_path;
 	GPContext *context = sample_create_context();
     FILE 	*f;
-    char	*data_from_camera;
-
-    char *data_to_open_cv;
+    char	*data;
 	unsigned long size;
     int retval;
 	
@@ -213,34 +209,39 @@ main(int argc, char **argv) {
 	retval = gp_camera_init(camera, context);
 	if (retval != GP_OK) {
 		printf("  Retval of capture_to_file: %d\n", retval);
+
 		exit (1);
 	}
 
     printf("Capturing To Memory\n");
-    capture_to_memory(camera, context, (const char**)&data_from_camera, &size);
+    capture_to_memory(camera, context, &camera_file_path, cameraFile, (const char**)&data, &size);
     printf("Captured To memory Writing to OpenCV Mat\n");
 
     Mat img_real = imread("foo2.jpg");
     
+    f = fopen("foo_test.jpg", "wb");
+	if (f) {
+		retval = fwrite (data, size, 1, f);
+		if (retval != (int)size) {
+			printf("  fwrite size %ld, written %d\n", size, retval);
+		}
+		fclose(f);
+	} else
+		printf("  fopen foo2.jpg failed.\n");
+
     cout << "Image Width: " << img_real.size().width << 
             ", Height" << img_real.size().height << 
             ", Type : " <<  type2str(img_real.type()) <<"\n";
   
     cout << "Displaying OpenCV image, size: " <<  size <<"\n";
 
-    // latest theory was invalidated pointer with just data by the time it gets to
-    // imshow, especially since it was largely nonsense.
-    // so now I'm copying to something don on the heap
-    // or else it's because the pointer died
-    // but I actually don't think that's it....
-    data_to_open_cv = (char*)malloc(size);
+    char *data_to_open_cv = (char*)malloc(size);
 
     for(int i = 0; i < size; i++)
     {
-        data_to_open_cv[i] = data_from_camera[i];
+        data_to_open_cv[i] = data[i];
     }
 
-    // I'm starting to suspect that the "data" is the issue
     Mat img = Mat(Size(6000, 4000), CV_8UC3, data_to_open_cv, Mat::AUTO_STEP);
     if(img.empty())
     {
@@ -248,10 +249,17 @@ main(int argc, char **argv) {
         return 1;
     }
 
+    Mat img_to_display;
+    resize(img, img_to_display, Size(600, 400));
 
     cout << "Displaying Window\n";
-    imshow("Display window", img); 
+    imshow("Display window", img_to_display); 
     waitKey(0);
+
+    printf("Deleting.\n");
+	retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
+			context);
+	printf("  Retval: %d\n", retval);
 
 	gp_camera_exit(camera, context);
     free(data_to_open_cv);
