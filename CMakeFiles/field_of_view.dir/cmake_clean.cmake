d4a1f10fd4f8b7fcf96ba0477778823ae3d5a8b4
seadav.17@gmail.com <seadav.17@gmail.com>
2022-09-04 20:43:08 -0700

    CMake and QT

---
+++ CMakeFiles/field_of_view.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/field_of_view.dir/main.cpp.o"
+  "CMakeFiles/field_of_view.dir/window.cpp.o"
+  "field_of_view"
+  "field_of_view.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/field_of_view.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
