864df28be0767cc19070c19715adc6393936108e
seadav.17@gmail.com <seadav.17@gmail.com>
2022-11-13 15:16:29 -0800

    moving functions to the cpp class

---
+++ src/camera/src/camera.h
@@ -0,0 +1,46 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <gphoto2/gphoto2.h>
+#include <gphoto2/gphoto2-camera.h>
+
+using namespace cv;
+
+class ICamera
+{
+
+    public:
+
+        /**
+         * connects to a camera.
+         * Just connects to the first camera that
+         * it can find
+         **/
+        virtual int connect() = 0;
+
+        /*
+        snaps a picutre and returns the snapped picture in an OpenCV matrix.
+        */
+        virtual Mat snap_picture() = 0;
+};
+
+
+
+class RemoteCamera : ICamera
+{
+	public:
+        RemoteCamera();
+		int connect() override;
+
+		Mat snap_picture() override;
+
+
+    private:
+        Camera	*camera;
+        CameraFile *camera_file;
+        GPContext *context;
+        char *buffer;
+        unsigned long buffer_size;
+
+};
--- src/ui/src/window.cpp
+++ src/ui/src/window.cpp
@@ -4,58 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include "camera_util.h"
+#include "camera.h"
 
 using namespace cv;
 using namespace std;
 
 void Window::handleButton(){
  
-  Camera	*camera;
-    CameraFile *cameraFile;
-    CameraFilePath camera_file_path;
-	GPContext *context = CameraUtil::sample_create_context();
-    FILE 	*f;
-    char* data;
-	unsigned long size;
-    int retval;
-	
+  auto camera = RemoteCamera();
+  cout << "Connecting Camera!\n";
+  if(!camera.connect())
+    return;
 
-	gp_log_add_func(GP_LOG_ERROR, CameraUtil::errordumper, NULL);
-	gp_camera_new(&camera); 
-	/* When I set GP_LOG_DEBUG instead of GP_LOG_ERROR above, I noticed that the
-	 * init function seems to traverse the entire filesystem on the camera.  This
-	 * is partly why it takes so long.
-	 * (Marcus: the ptp2 driver does this by default currently.)
-	 */
-	printf("Camera init.  Takes about 10 seconds.\n");
-	retval = gp_camera_init(camera, context);
-	if (retval != GP_OK) {
-		printf("  Retval of gp_camera_init: %d\n", retval);
-
-		exit (1);
-	}
-
-    printf("Capturing To Memory\n");
-    CameraUtil::capture_to_memory(camera, context, &camera_file_path, cameraFile, (const char**)&data, &size);
-    printf("Captured To memory Writing to OpenCV Mat\n");
-
-    cout << "Displaying OpenCV image, size:  " <<  size << "\n";
-
-    Mat img = imdecode(Mat(1, size, CV_8UC1, data), CV_LOAD_IMAGE_UNCHANGED);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image From the camera: ";
-        return;
-		
-	  }
-
+  Mat img =  camera.snap_picture();
 	Mat out;
 	cv::resize(img, out, cv::Size(480, 320));
 
 
   cout << "Image Total:  " << img.total() << " Element Size: " << img.elemSize() << "\n";
-  cout << "Format: " << type2str(img.type()) << "\n";
   cvtColor(out,out,CV_BGR2RGB);
   pixmap = new QPixmap();
   image = new QImage(out.data, out.cols, out.rows, QImage::Format_RGB888);
