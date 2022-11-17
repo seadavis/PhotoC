80bce8148ae9385099b76fdc651c032d29291ff4
seadav.17@gmail.com <seadav.17@gmail.com>
2022-11-16 18:29:09 -0800

    fixing formatting issues

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,13 @@
 
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
 
- Window window;
- window.show();
 
- return app.exec();
+
+    QApplication app (argc, argv);
+
+    Window window;
+    window.show();
+
+    return app.exec();
 }
