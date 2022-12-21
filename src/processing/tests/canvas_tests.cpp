c5cc993c9bf9f5180db755245809994fef5ed8b8
seadav.17@gmail.com <seadav.17@gmail.com>
2022-12-21 09:10:45 -0800

    adding tests. doesn't compile using this as a checkpoint

--- src/processing/tests/canvas_tests.cpp
+++ src/processing/tests/canvas_tests.cpp
@@ -13,9 +13,30 @@ class Composites :
     public testing::TestWithParam<tuple<string, string, int, int>> {
 };
 
+
 class CompositeMissingOneImage :
-  public testing::TestWithParam<tuple<string, string, int, int>> {
+  public testing::TestWithParam<tuple<string, string, string, int, int, string>> {
 };
+
+Mat loadBackgroundImage(string path);
+
+INSTANTITATE_TEST_SUITE_P(CompositeCanvas,
+                          CompositeMissingOneImage,
+                          testing::Values(
+                                make_tuple("", nullptr, nullptr, 800, 750, "both_composites_nullptr.png"),
+                                make_tuple("", "", nullptr, 650, 400, "backkground_empty_mask_empty_original_null.png"),
+                                make_tuple(nullptr, "", "", 1300, 1150, "background_null_others_empty.png"),
+                                make_tuple("","", "", 1250, 975, "all_empty.png"),
+                                make_tuple(nullptr, nullptr, nullptr, 625, 350, "all_null.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png","./src/processing/tests/masks/balloon.png", "", 1050, 950, "mask_empty.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png","", "./src/processing/tests/original_source_images/balloon.png", 450, 300, "original_empty.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png","./src/processing/tests/masks/balloon.png", nullptr, 1050, 950, "original_null.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png",nullptr, "./src/processing/tests/original_source_images/balloon.png", 450, 300, "mask_null.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png",nullptr, nullptr, 450, 300, "mask_and_original_null.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png","", "", 1150, 1150, "mask_and_original_empty.png"),
+                                make_tuple("./src/processing/tests/target_images/library.png","./src/processing/tests/masks/balloon.png", "./src/processing/tests/original_source_images/penguin.png", 1050, 950, "mismtach.png")
+                          ) );
+
 
 INSTANTIATE_TEST_SUITE_P(CompositeCanvas,
                          Composites,
@@ -109,14 +130,56 @@ TEST(CompositeCanvas, BoundingRect)
 
 }
 
+
+TEST(CompositeCanvas, OnlyCompositesSet)
+{
+    auto backgroundImage = loadBackgroundImage("./src/processing/tests/target_images/liberty.png");
+    auto canvas = CompositeCanvas();
+    canvas.setSize(800, 450);
+    canvas.setComposite("./src/processing/tests/masks/moon.png", "./src/processing/tests/original_source_images/moon.png");
+
+    Mat result = canvas.currentImg();
+
+    imwrite("./src/processing/tests/target_missing_images/only_background_set.png", result);
+}
+
+TEST(CompositeCanvas, OnlyBackgroundSet)
+{
+    auto backgroundImage = loadBackgroundImage("./src/processing/tests/target_images/liberty.png");
+    auto canvas = CompositeCanvas();
+    canvas.setSize(800, 450);
+    canvas.setBackground(backgroundImage);
+
+    Mat result = canvas.currentImg();
+
+    imwrite("./src/processing/tests/target_missing_images/only_background_set.png", result);
+}
+
+TEST_P(CompositeMissingOneImage, MissingOrMismatching)
+{
+    auto args = GetParam();
+    auto backgroundImage = loadBackgroundImage(get<0>(args));
+    auto maskPath = get<1>(args);
+    auto originalPath = get<2>(args);
+  
+    auto canvas = CompositeCanvas();
+    canvas.setSize(get<3>(args), get<4>(args));
+    canvas.setBackground(backgroundImage);
+    canvas.setComposite(maskPath, originalPath);
+
+    Mat result = canvas.currentImg();
+
+    imwrite("./src/processing/tests/target_missing_images/" + get<5>(args), result);
+
+    /*bool const equal = std::equal(result.begin<uchar>(), result.end<uchar>(), expectedMat.begin<uchar>());
+    ASSERT_TRUE(equal);*/
+}
+
 TEST_P(Composites, BasicComposite) {
 
   auto args = GetParam();
   auto backgroundImagePath = "./src/processing/tests/target_images/" + get<1>(args) + ".png";
-  auto backgroundImageRGB = imread(backgroundImagePath, CV_LOAD_IMAGE_UNCHANGED);
-
-  Mat backgroundImage;
-  cvtColor(backgroundImageRGB, backgroundImage, CV_BGR2BGRA);
+  auto backgroundImage = loadBackgroundImage(backgroundImagePath);
 
   auto mask = "./src/processing/tests/masks/" + get<0>(args) + ".png";
   auto original = "./src/processing/tests/original_source_images/" + get<0>(args) + ".png";
@@ -135,4 +198,13 @@ TEST_P(Composites, BasicComposite) {
 
   bool const equal = std::equal(result.begin<uchar>(), result.end<uchar>(), expectedMat.begin<uchar>());
   ASSERT_TRUE(equal);
+}
+
+Mat loadBackgroundImage(string path)
+{
+  auto backgroundImageRGB = imread(path, CV_LOAD_IMAGE_UNCHANGED);
+
+  Mat backgroundImage;
+  cvtColor(backgroundImageRGB, backgroundImage, CV_BGR2BGRA);
+  return backgroundImage;
 }
