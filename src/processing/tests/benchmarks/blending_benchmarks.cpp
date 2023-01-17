9726509d90e48f5450cb31d5e0e3877e68bbc4f8
seadav.17@gmail.com <seadav.17@gmail.com>
2023-01-16 19:54:04 -0800

    getting single benchmark to run from script

--- src/processing/tests/benchmarks/blending_benchmarks.cpp
+++ src/processing/tests/benchmarks/blending_benchmarks.cpp
@@ -1,22 +1,51 @@
 #include <benchmark/benchmark.h>
 #include "processing.h"
 #include <iostream>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+using namespace processing;
+using namespace std;
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+Mat loadBackgroundImage(string path)
+{
+  auto backgroundImageRGB = imread(path, CV_LOAD_IMAGE_UNCHANGED);
+
+  Mat backgroundImage;
+  cvtColor(backgroundImageRGB, backgroundImage, CV_BGR2BGRA);
+  return backgroundImage;
 }
-BENCHMARK(BM_StringCopy);
+
+static void BM_CompositeCalculations(benchmark::State& state) {
+
+    auto backgroundImagePath = "./src/processing/tests/target_images/beach_large.png";
+    auto backgroundImage = loadBackgroundImage(backgroundImagePath);
+
+    auto mask = "./src/processing/tests/masks/eagle_mask_large.png";
+    auto original = "./src/processing/tests/original_source_images/eagle_large.png";
+
+    auto canvas = CompositeCanvas();
+    canvas.setSize(2400, 1600);
+    canvas.setBackground(backgroundImage);
+    canvas.setComposite(mask, original);
+
+    Mat result;
+
+    for (auto _ : state) {
+        // This code gets timed
+        result = canvas.currentImg();
+    }
+
+    result = canvas.currentImg();
+    string fileName =  "large_eagle_results.png";
+    imwrite( "./src/processing/tests/benchmark_results/" + fileName, result);
+
+}
+
+
+// Register the function as a benchmark
+BENCHMARK(BM_CompositeCalculations);
+
 
 BENCHMARK_MAIN();
