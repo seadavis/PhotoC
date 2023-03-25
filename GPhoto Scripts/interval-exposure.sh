916b1206b5317e8feff06d0955b6009109701492
seadav.17@gmail.com <seadav.17@gmail.com>
2023-03-25 12:34:23 -0700

    building long exposure script

---
+++ GPhoto Scripts/interval-exposure.sh
@@ -0,0 +1,17 @@
+ SECONDS=$1
+ INTERVALS=$2
+ FOLDERPATH=$3
+
+ for (( c=0; c<$INTERVALS; c++ ))
+ do
+    SUFFIX=".jpg"
+    DATE=$(date '+%Y%m%d')
+    FILENAME="$FOLDERPATH$DATE"_"$SECONDS"s""_"$INTERVALS"I""_"$c$SUFFIX"
+
+    if [ ${#FOLDERPATH} = 0 ]
+    then
+        gphoto2 ---set-config bulb=1 --wait-event=$1 --set-config bulb=0
+    else
+        gphoto2 --filename=$FILENAME --set-config bulb=1 --wait-event=$1 --set-config bulb=0 --wait-event-and-download=1s
+    fi
+done
