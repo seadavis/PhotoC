88855b6dd0a5e04751e24e3c4de12f542f2ed588
seadav.17@gmail.com <seadav.17@gmail.com>
2023-03-30 17:34:07 -0700

    fixing error in interval exposure script

--- GPhoto Scripts/interval-exposure.sh
+++ GPhoto Scripts/interval-exposure.sh
@@ -10,7 +10,7 @@
 
     if [ ${#FOLDERPATH} = 0 ]
     then
-        gphoto2 ---set-config bulb=1 --wait-event=$1 --set-config bulb=0
+        gphoto2 --set-config bulb=1 --wait-event=$1 --set-config bulb=0 --wait-event=1ms
     else
         gphoto2 --filename=$FILENAME --set-config bulb=1 --wait-event=$1 --set-config bulb=0 --wait-event-and-download=1s
     fi
