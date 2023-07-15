16aa4b185c845542ade84097aad8ef5bf1df6cff
seadav.17@gmail.com <seadav.17@gmail.com>
2023-07-14 21:16:11 -0700

    eliminating functions and structures that fell into disuse, after stacker optimizations

--- src/blending/src/blending_common.cpp
+++ src/blending/src/blending_common.cpp
@@ -1,15 +1,6 @@
 #include "blending_common.h"
 
-double pixel_brightness(Vec4b& v)
-{
-    return pixel_brightness(v[0], v[1], v[2]);
-}
-
 double pixel_brightness(uchar r, uchar g, uchar b)
 {
      return sqrt(pow(r, 2) + pow(g, 2) + pow(b, 2));
-}
-
-void in_place_sort(vector<PixelBrightnessMeasure>& original) {
-    sort(original.begin(), original.end(), less_than_key());
 }
--- src/blending/src/blending_common.h
+++ src/blending/src/blending_common.h
@@ -12,30 +12,9 @@ class IStackImages
         virtual Mat GetCurrentBlend() = 0;
 };
 
-
-struct PixelBrightnessMeasure
-{
-    int index;
-    double measure;
-
-    PixelBrightnessMeasure(int index, double measure) : index(index), measure(measure){};
-
-    PixelBrightnessMeasure(){};
-};
-
-struct less_than_key
-{
-    inline bool operator() (const PixelBrightnessMeasure& struct1, const PixelBrightnessMeasure& struct2)
-    {
-        return (struct1.measure < struct2.measure);
-    }
-};
-
 double pixel_brightness(Vec4b& v);
 
 double pixel_brightness(uchar r, uchar g, uchar b);
-
-void in_place_sort(vector<PixelBrightnessMeasure>& original);
 
 class StackResizedException : public std::exception {
         public:
